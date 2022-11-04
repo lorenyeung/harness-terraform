f08a0ebab92d51a6e5d79afcca5eb202480d300b
Loren Yeung <loren.w.yeung@gmail.com>
2022-11-03 17:13:40 -0700

    more user group changes

--- rbac.tf
+++ rbac.tf
@@ -1,52 +1,37 @@
 resource "harness_platform_usergroup" "project_admin" {
+  org_id                    = var.org[count.index]
+  project_id                = var.project[count.index]  
   identifier = "${var.project[count.index]}_admin"
   name        = "${var.project[count.index]}_admin"
   description = "Administrators of the ${var.project[count.index]} project."  
   count = length(var.project)
-#   permissions {
-
-#     account_permissions = [
-#       "CREATE_CUSTOM_DASHBOARDS"
-#     ]
-
-#     app_permissions {
-
-#       all {
-#         app_ids = [harness_application.demo.id]
-#         actions = ["CREATE", "READ", "UPDATE", "DELETE"]
-#       }
-
-#     }
-#   }
 }
+
+# resource "harness_platform_role_assignments" "example" {
+#   identifier                = "${var.project[count.index]}_admin_role_assign"
+# //  org_id                    = var.org[count.index]
+# //  project_id                = var.project[count.index]
+#   resource_group_identifier = "_all_account_level_resources"
+#   role_identifier           = "_account_viewer"
+#   principal {
+#     identifier = "${var.project[count.index]}_admin"
+#     type       = "USER_GROUP"
+#   }
+#   disabled = false
+#   managed  = false
+#   count = length(var.project)
+#   depends_on = [
+#     harness_platform_usergroup.project_admin
+#   ]
+# }
+
 
 resource "harness_platform_usergroup" "project_developer" {
   identifier = "${var.project[count.index]}_dev"
   name        = "${var.project[count.index]}_dev"
   description = "Developers of the ${var.project[count.index]} project."
   count = length(var.project)
-#   permissions {
-#     account_permissions = [
-#       "CREATE_CUSTOM_DASHBOARDS"
-#     ]
-
-#     app_permissions {
-
-#       all {
-#         app_ids = [harness_application.demo.id]
-#         actions = ["READ"]
-#       }
-
-#       deployment {
-#         app_ids = [harness_application.demo.id]
-#         filters = ["NON_PRODUCTION_ENVIRONMENTS"]
-#         actions = ["READ", "ROLLBACK_WORKFLOW", "EXECUTE_PIPELINE", "EXECUTE_WORKFLOW"]
-#       }
-
-#     }
-#   }
 }
-
 
 //admin - project admin
 //dev - can deploy to project, not approve
