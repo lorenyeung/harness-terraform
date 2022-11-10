b794a2b9a8089554a6b65501a078c716574117bf
Loren Yeung <loren.w.yeung@gmail.com>
2022-11-10 11:21:32 -0800

    removing s3

--- harness/state.tf
+++ harness/state.tf
@@ -4,45 +4,14 @@ resource "aws_kms_key" "terraform-bucket-key" {
  enable_key_rotation     = true
 }
 
-resource "aws_kms_alias" "key-alias" {
- name          = "alias/terraform-bucket-key-loren"
- target_key_id = aws_kms_key.terraform-bucket-key.key_id
-}
-
-resource "aws_s3_bucket" "terraform-state" {
- bucket = var.s3_bucket_name
- acl    = "private"
-
- versioning {
-   enabled = true
- }
-
- server_side_encryption_configuration {
-   rule {
-     apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
-       sse_algorithm     = "aws:kms"
-     }
-   }
- }
-}
-
-resource "aws_s3_bucket_public_access_block" "block" {
- bucket = aws_s3_bucket.terraform-state.id
-
- block_public_acls       = true
- block_public_policy     = true
- ignore_public_acls      = true
- restrict_public_buckets = true
-}
-
 terraform {
  backend "s3" {
    //bucket         = local.BUCKET_NAME
-   key            = "state/terraform.tfstate<+pipeline.identifier><+pipeline.sequenceId>" //this needs to be dynamic and unique
+   //key            = "state/terraform.tfstate<+pipeline.identifier><+pipeline.sequenceId>" //this needs to be dynamic and unique
    region         = "us-west-1"
    encrypt        = true
    kms_key_id     = "alias/aws/s3"
    //dynamodb_table = "terraform-state"
  }
 }
+
