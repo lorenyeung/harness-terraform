3bf70347f0b2b90577f0d288883254eb66a5161d
Loren Yeung <loren.w.yeung@gmail.com>
2022-11-11 14:27:22 -0800

    refactor for modules 5 - s3

--- harness/modules/organization/provider.tf
+++ harness/modules/organization/provider.tf
@@ -3,6 +3,9 @@ terraform {
     harness = {
       source = "harness/harness"
     }
+    aws = {
+     source  = "hashicorp/aws"
+    }
   }
 }
 
@@ -10,4 +13,8 @@ provider "harness" {
   endpoint         = "https://app.harness.io/gateway"
   account_id       = var.harness_account_id
   platform_api_key = var.harness_apikey
+}
+
+provider "aws" {
+  region = "us-west-1"
 }
