c0965336439529a6db6eb3bd77e6e94bdac36bb4
Loren Yeung <loren.w.yeung@gmail.com>
2023-12-20 16:33:10 -0800

    terragrunt fix 5

--- 17/terragrunt/main.tf
+++ 17/terragrunt/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    harness = {
+      source = "harness/harness"
+      version = "0.29.4"
+    }
+  }
+}
+
 provider "harness" {
   endpoint         = var.harness_platform_url
   account_id       = var.harness_platform_account
